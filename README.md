cc92dc9cda8f305576da0d32b77abf461375c9b7
Mlie <emipa606@users.noreply.github.com>
2022-10-13 22:39:38 +0200

    Mod updated for 1.4 and passed autotests

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # WildKobolds
 
-![Image](https://i.imgur.com/WAEzk68.png)
+![Image](https://i.imgur.com/buuPQel.png)
 
 Update of Mozs mod
 https://steamcommunity.com/sharedfiles/filedetails/?id=941997958
 
-![Image](https://i.imgur.com/7Gzt3Rg.png)
+![Image](https://i.imgur.com/pufA0kM.png)
 
 	
-![Image](https://i.imgur.com/NOW7jU1.png)
+![Image](https://i.imgur.com/Z4GOv8H.png)
 
 This mod adds kobold animals into the game. Adorabold!
 
@@ -16,7 +16,7 @@ Kobolds are advanced-intelligence animals, which like to mine whatever they come
 
 Original kobold art belongs to twitch.tv/distractedelf
 
-![Image](https://i.imgur.com/Rs6T6cr.png)
+![Image](https://i.imgur.com/PwoNOj4.png)
 
 
 
@@ -26,6 +26,7 @@ Original kobold art belongs to twitch.tv/distractedelf
 -  For best support, please use the Discord-channel for error-reporting.
 -  Do not report errors by making a discussion-thread, I get no notification of that.
 -  If you have the solution for a problem, please post it to the GitHub repository.
+
 
 
 
--- Source/WildKobold/ThinkNode_ConditionalKobold.cs
+++ Source/WildKobold/ThinkNode_ConditionalKobold.cs
@@ -3,23 +3,22 @@ using RimWorld;
 using Verse;
 using Verse.AI;
 
-namespace WildKobold
-{
-    internal class ThinkNode_ConditionalKobold : ThinkNode_Conditional
-    {
-        protected override bool Satisfied(Pawn pawn)
-        {
-            // If we're dealing with a kobold.
-            if (pawn.def != null && pawn.def.defName == "Kobold" && !pawn.GetRegion().touchesMapEdge)
-                // Return true if the kobold is untrained or outside of the home area.
-            {
-                return pawn.training == null ||
-                       !pawn.training.HasLearned(DefDatabase<TrainableDef>.GetNamed("Tameness")) ||
-                       !pawn.Map.areaManager.Home.ActiveCells.Contains(pawn.Position);
-            }
+namespace WildKobold;
 
-            // The pawn isn't an eligible kobold - fail.
-            return false;
+internal class ThinkNode_ConditionalKobold : ThinkNode_Conditional
+{
+    protected override bool Satisfied(Pawn pawn)
+    {
+        // If we're dealing with a kobold.
+        if (pawn.def != null && pawn.def.defName == "Kobold" && !pawn.GetRegion().touchesMapEdge)
+            // Return true if the kobold is untrained or outside of the home area.
+        {
+            return pawn.training == null ||
+                   !pawn.training.HasLearned(DefDatabase<TrainableDef>.GetNamed("Tameness")) ||
+                   !pawn.Map.areaManager.Home.ActiveCells.Contains(pawn.Position);
         }
+
+        // The pawn isn't an eligible kobold - fail.
+        return false;
     }
 }
